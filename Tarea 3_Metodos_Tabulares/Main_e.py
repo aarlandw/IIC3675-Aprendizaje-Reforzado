fe0a346d078f92cbc8818ba197f34fcefceb6967
William Aarland <aarlandw@gmail.com>
2025-05-13 12:57:42 -0400

    Add: readme, and report. Restructure of project

--- Tarea 3_Metodos_Tabulares/Main_e.py
+++ Tarea 3_Metodos_Tabulares/Main_e.py
@@ -2,13 +2,11 @@ import numpy as np
 from Environments.MultiGoalEnvs.RoomEnv import RoomEnv
 from collections import defaultdict
 import matplotlib.pyplot as plt
-from tqdm import tqdm
+from tqdm import tqdm, trangera
 import csv
 
 
-
 def epsilon_greedy_action(Q, state, goal, action_space, epsilon):
-
     n_actions = len(env.action_space)
     if np.random.random() < epsilon:
         return np.random.randint(n_actions)
@@ -18,16 +16,15 @@ def epsilon_greedy_action(Q, state, goal, action_space, epsilon):
 ## Versiones estándar (sin multi-goal)
 
 def q_learning(env, alpha=0.1, gamma=0.99, epsilon=0.1, num_episodes=500):
-
     episode_lengths = np.zeros(num_episodes)
     n_actions = len(env.action_space)
     action_map = {i: a for i, a in enumerate(env.action_space)}
     
     Q = defaultdict(lambda: np.ones(len(env.action_space)))
     
-    for episode in tqdm(range(num_episodes)):
+    for episode in range(num_episodes):
         state = env.reset()
-        current_goal = state[1]  # El objetivo está en el estado
+        current_goal = state[1] 
         done = False
         steps = 0
         
@@ -38,7 +35,7 @@ def q_learning(env, alpha=0.1, gamma=0.99, epsilon=0.1, num_episodes=500):
             next_goal = next_state[1]
             steps += 1
             
-            # Actualización Q-learning estándar
+            
             current_q = Q[(state, current_goal)][a]
             max_next_q = max(Q[(next_state, current_goal)]) if not done else 0
             Q[(state, current_goal)][a] = current_q + alpha * (reward + gamma * max_next_q - current_q)
@@ -48,74 +45,148 @@ def q_learning(env, alpha=0.1, gamma=0.99, epsilon=0.1, num_episodes=500):
         episode_lengths[episode] = steps
 
     return episode_lengths
+def sarsa(env, num_episodes, alpha=0.1, gamma=0.99, epsilon=0.1):
 
-def sarsa(env, alpha=0.1, gamma=0.99, epsilon=0.1, num_episodes=500, n_steps=1):
-   
-    episode_lengths = np.zeros(num_episodes)
-    n_actions = len(env.action_space)
-    action_map = {i: a for i, a in enumerate(env.action_space)}
-
-    Q = defaultdict(lambda: np.ones(len(env.action_space)))
+    Q = defaultdict(lambda: np.ones(len(env.action_space)))  
+    episode_lengths = []
+    action_list = list(env.action_space)
     
-    for episode in tqdm(range(num_episodes)):
+    for episode in range(num_episodes):
+        # Inicialización del episodio
         state = env.reset()
-        current_goal = state[1]
+        current_goal = state[1]  
+        current_state = (state, current_goal)
+        
+ 
+        if np.random.rand() < epsilon:
+            action_idx = np.random.randint(len(action_list))
+        else:
+            action_idx = np.argmax(Q[current_state])
+        
         done = False
         steps = 0
-        history = []
         
-        # Selección acción inicial
-        a = epsilon_greedy_action(Q, state, current_goal, env.action_space, epsilon)
-
         while not done:
-            action = action_map[a]
+            # Ejecutar acción
+            action = action_list[action_idx]
             next_state, reward, done = env.step(action)
             next_goal = next_state[1]
+            next_state_tuple = (next_state, next_goal)
             steps += 1
             
-            # Guardar experiencia
-            history.append(((state, current_goal), a, reward))
+    
+            if not done:
+                if np.random.rand() < epsilon:
+                    next_action_idx = np.random.randint(len(action_list))
+                else:
+                    next_action_idx = np.argmax(Q[next_state_tuple])
+            else:
+                next_action_idx = None
             
-            # Selección siguiente acción
-            next_a = epsilon_greedy_action(Q, next_state, next_goal, env.action_space, epsilon) if not done else None
+            # Actualización SARSA solo para el goal actual
+            if not done:
+                
+                Q[current_state][action_idx] += alpha * (
+                    reward + gamma * Q[next_state_tuple][next_action_idx] - Q[current_state][action_idx]
+                )
+            else:
+             
+                Q[current_state][action_idx] += alpha * (reward - Q[current_state][action_idx])
             
-            # Actualización n-step Sarsa
-            if len(history) >= n_steps or done:
-                tau = len(history) - n_steps
-                if tau < 0: tau = 0
-                
-                # Calcular retorno n-step
-                G = sum(gamma**(i - tau) * history[i][2] for i in range(tau, min(tau + n_steps, len(history))))
-                
-                if tau + n_steps < len(history):
-                    s_n, a_n = history[tau + n_steps][0], history[tau + n_steps][1]
-                    G += gamma**n_steps * Q[s_n][a_n]
-                
-                # Actualizar Q-value
-                s_tau, a_tau = history[tau][0], history[tau][1]
-                Q[s_tau][a_tau] += alpha * (G - Q[s_tau][a_tau])
-                
-                # Eliminar experiencia antigua
-                if len(history) > n_steps:
-                    history.pop(0)
-            
-            state, current_goal, a = next_state, next_goal, next_a
+            # Transición al siguiente estado
+            current_state = next_state_tuple
+            action_idx = next_action_idx
         
-        episode_lengths[episode] = steps
+        episode_lengths.append(steps)
+    
+    return np.array(episode_lengths)
 
-    return episode_lengths
+def n_step_sarsa(env, num_episodes, alpha, gamma, epsilon, n=8):
+    Q = defaultdict(lambda: np.ones(len(env.action_space)))  
+    episode_lengths = []
+    
+    for episode in range(num_episodes):
+        
+        S = [None] * (n + 1)  
+        A = [None] * (n + 1) 
+        R = [None] * (n + 1)  
+        # Inicialización del episodio
+        state = env.reset()
+        current_goal = state[1]  
+        S[0] = (state, current_goal)
+        
 
+        if np.random.rand() < epsilon:
+            A[0] = np.random.randint(len(env.action_space))
+        else:
+            A[0] = np.argmax(Q[S[0]])
+        
+        T = float('inf')  
+        t = 0
+        steps = 0
+        
+        while True:
+            if t < T:
+                # Ejecutar acción
+                action = env.action_space[A[t % (n + 1)]]
+                next_state, reward, done = env.step(action)
+                next_goal = next_state[1]
+                
+            
+                S[(t + 1) % (n + 1)] = (next_state, next_goal)
+                R[(t + 1) % (n + 1)] = reward
+                steps += 1
+                
+                if done:
+                    T = t + 1
+                else:
+                   
+                    if np.random.rand() < epsilon:
+                        A[(t + 1) % (n + 1)] = np.random.randint(len(env.action_space))
+                    else:
+                        A[(t + 1) % (n + 1)] = np.argmax(Q[S[(t + 1) % (n + 1)]])
+            
+            tau = t - n + 1 
+            
+            if tau >= 0:
+                # Calcular retorno n-step
+                G = sum(
+                    gamma**(i - tau - 1) * R[i % (n + 1)] 
+                    for i in range(tau + 1, min(tau + n + 1, T + 1))
+                )
+                
+        
+                if tau + n < T:
+                    s_n = S[(tau + n) % (n + 1)]
+                    a_n = A[(tau + n) % (n + 1)]
+                    G += gamma**n * Q[s_n][a_n]
+                
+                # Actualizar Q-value solo para el goal actual
+                s_tau = S[tau % (n + 1)]
+                a_tau = A[tau % (n + 1)]
+                Q[s_tau][a_tau] += alpha * (G - Q[s_tau][a_tau])
+            
+            if tau == T - 1:
+                break
+                
+            t += 1
+        
+        episode_lengths.append(steps)
+    
+    return np.array(episode_lengths)
+
+    
 ## Versiones multi-goal
 
 def multi_goal_q_learning(env, alpha=0.1, gamma=0.99, epsilon=0.1, num_episodes=500):
-
+    """Q-learning con actualización multi-goal (solo un run)"""
     episode_lengths = np.zeros(num_episodes)
     n_actions = len(env.action_space)
     action_map = {i: a for i, a in enumerate(env.action_space)}  
 
     Q = defaultdict(lambda: np.ones(len(env.action_space)))
     
-    for episode in tqdm(range(num_episodes)):
+    for episode in range(num_episodes):
         state = env.reset()
         current_goal = state[1]
         done = False
@@ -141,59 +212,68 @@ def multi_goal_q_learning(env, alpha=0.1, gamma=0.99, epsilon=0.1, num_episodes=
     return episode_lengths
 
 def multi_goal_sarsa(env, alpha=0.1, gamma=0.99, epsilon=0.1, num_episodes=500):
-
-    episode_lengths = np.zeros(num_episodes)
-    action_list = list(env.action_space)
-    action_size = len(action_list)
-
-    Q = defaultdict(lambda: np.ones(action_size))
+    Q = defaultdict(lambda: np.ones(len(env.action_space)))  
+    episode_lengths = []
+    n_actions = len(env.action_space)
+    action_map = {i: a for i, a in enumerate(env.action_space)}
     
     for episode in range(num_episodes):
         state = env.reset()
-        current_goal = state[1]
+        current_goal = state[1]  
         done = False
         steps = 0
-
-        # Selección acción inicial
-        if np.random.random() < epsilon:
-            current_action_idx = np.random.randint(action_size)
+        
+        
+        if np.random.rand() < epsilon:
+            a_idx = np.random.randint(n_actions)
         else:
-            current_action_idx = np.argmax(Q[(state, current_goal)])
-
+            a_idx = np.argmax(Q[(state, current_goal)])
+        
         while not done:
-            action = action_list[current_action_idx]
+            action = action_map[a_idx]
             next_state, reward, done = env.step(action)
             next_goal = next_state[1]
             steps += 1
-
+            
+            # Selección siguiente acción ε-greedy para el nuevo goal
             if not done:
-                if np.random.random() < epsilon:
-                    next_action_idx = np.random.randint(action_size)
+                if np.random.rand() < epsilon:
+                    a_idx_next = np.random.randint(n_actions)
                 else:
-                    next_action_idx = np.argmax(Q[(next_state, next_goal)])
+                    a_idx_next = np.argmax(Q[(next_state, next_goal)])
             else:
-                next_action_idx = None
-
+                a_idx_next = None
+            
+            # Actualización para TODOS los goals (multi-goal)
             for goal in env.goals:
                 if goal == current_goal:
-                    target_action_idx = next_action_idx
+                    target_action = a_idx_next
                 else:
-                    if np.random.random() < epsilon:
-                        target_action_idx = np.random.randint(action_size)
+                    # Para otros goals, seleccionamos acción ε-greedy
+                    if np.random.rand() < epsilon:
+                        target_action = np.random.randint(n_actions)
                     else:
-                        target_action_idx = np.argmax(Q[(next_state, goal)])
-
-                target_q = 0 if done else Q[(next_state, goal)][target_action_idx]
-
-                Q[(state, goal)][current_action_idx] += alpha * (
-                    reward + gamma * target_q - Q[(state, goal)][current_action_idx]
+                        target_action = np.argmax(Q[(next_state, goal)])
+                
+                # Calcular target Q-value
+                if done:
+                    target_q = 0 
+                else:
+                    target_q = Q[(next_state, goal)][target_action]
+                
+                # Actualizar Q-value para este goal
+                Q[(state, goal)][a_idx] += alpha * (
+                    reward + gamma * target_q - Q[(state, goal)][a_idx]
                 )
+            
+            # Avanzar al siguiente estado
+            state, current_goal, a_idx = next_state, next_goal, a_idx_next
+        
+        episode_lengths.append(steps)
+    
+    return np.array(episode_lengths)
 
-            state, current_goal, current_action_idx = next_state, next_goal, next_action_idx
 
-        episode_lengths[episode] = steps
-
-    return episode_lengths
 
 if __name__ == "__main__":
     env = RoomEnv()
@@ -213,10 +293,10 @@ if __name__ == "__main__":
 
     print("Ejecutando experimentos...")
     runs = 100
-    for i in tqdm(range(runs)):
+    for i in trange(runs):
+        all_s.append(sarsa(env, **params))
         all_q.append( q_learning(env, **params))
-        all_s.append( sarsa(env, n_steps=1, **params))
-        all_s8.append(sarsa(env, n_steps=8, **params))
+        all_s8.append(n_step_sarsa(env, n=8, **params))
         all_qmult.append( multi_goal_q_learning(env, **params))
         all_smult.append( multi_goal_sarsa(env, **params))
 
@@ -229,7 +309,7 @@ if __name__ == "__main__":
 
 
     # Guardar resultados en CSV
-    with open("largo_episodio_e.csv", "w", newline="") as f:
+    with open("largo_episodio_e2.csv", "w", newline="") as f:
         writer = csv.writer(f)
         # Escribir encabezado
         writer.writerow(["Episodio", "Q-learning", "Sarsa (1-step)", "Sarsa (8-step)", "Multi-goal Q-learning", "Multi-goal Sarsa"])
